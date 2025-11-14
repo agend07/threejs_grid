2e739b947c764979a835fdcd6c5a0314328a8ce9
agend07 <agend07@gmail.com>
2025-11-14 23:11:53 +0000

    grid

--- index.js
+++ index.js
@@ -1,32 +1,85 @@
 import * as THREE from 'three';
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
+import { EffectComposer } from "jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "jsm/postprocessing/RenderPass.js";
+import { GTAOPass } from 'jsm/postprocessing/GTAOPass.js';
+import { OutputPass } from 'jsm/postprocessing/OutputPass.js';
 
 const width = window.innerWidth, height = window.innerHeight;
 
 // init
-
-const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
-camera.position.z = 1;
+const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 40);
+camera.position.z = 6;
+camera.position.y = -2;
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-const material = new THREE.MeshNormalMaterial();
+const boxGroup = new THREE.Group();
+scene.add(boxGroup);
+boxGroup.update = (t) => {
+	boxGroup.rotation.z = t;
+};
 
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
+const geometry = new THREE.BoxGeometry();
+const spacing = 1.1;
+const palette = [0x780000, 0xc1121f, 0xfdf0d5, 0x003049, 0x669bbc];
 
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize( width, height );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+const startPos = {x: -2, y: -2,	z: 0};
+
+function createBox({ color = 0x00ff00, index = 0, x = 0, y = 0, z = 0, scale = 1 }) {
+	const material = new THREE.MeshStandardMaterial({ color })
+	const mesh = new THREE.Mesh(geometry, material);
+	mesh.position.x = startPos.x + x * spacing;
+	mesh.position.y = startPos.y + y * spacing;
+	mesh.position.z = z + (Math.floor(Math.random() * 3) + 1) * 0.2;
+	mesh.rotation.z = Math.PI * 0.25;
+
+	mesh.scale.setScalar(scale);
+	return mesh;
+}
+
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(width, height);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+const ctrls = new OrbitControls(camera, renderer.domElement);
+ctrls.enableDamping = true;
+
+
+const gridSize = 14;
+for (let x = 0; x < gridSize; x += 1) {
+	for (let y = 0; y < gridSize; y += 1) {
+		const hex = palette[Math.floor(Math.random() * palette.length)];
+		const color = new THREE.Color(hex);
+		const scale = 1.1 + Math.random() * 0.2 - 0.1;
+		const z = -1 + Math.random() * 0.2 - 0.1;
+		const box = createBox({ color, index: x + y, x, y, z, scale });
+		boxGroup.add(box);
+	}
+}
+
+scene.add(boxGroup);
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 5);
+scene.add(hemiLight);
+
+const pointlight = new THREE.PointLight(0xffffff, 3);
+
+pointlight.position.x = 1;
+pointlight.position.z = 2;
+scene.add(pointlight);
 
 // animation
 
-function animate( time ) {
+function animate(time) {
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
+	// mesh.rotation.x = time / 2000;
+	// mesh.rotation.y = time / 1000;
 
-	renderer.render( scene, camera );
+	renderer.render(scene, camera);
+
+	ctrls.update();
 
 }
